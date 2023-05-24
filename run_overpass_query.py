5663707b75019126d94a10c4b6cfd691754c7d32
Dan Vanderkam <danvdk@gmail.com>
2023-05-24 11:39:37 -0400

    More logging, narrow down peaks query

--- run_overpass_query.py
+++ run_overpass_query.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 
 import sys
+import json
 from pathlib import Path
+from typing import List
+from collections import Counter
 
 import requests
 
@@ -12,11 +15,19 @@ def fetch_from_overpass(query: str) -> str:
     return r.text
 
 
+def element_stats(elements: List[any]):
+    return Counter(e['type'] for e in elements)
+
+
 for path in sys.argv[1:]:
     p = Path(path).name
     query = open(path).read()
     print(f'Running query {path}')
     result = fetch_from_overpass(query)
     with open(Path('data') / p.replace('.txt', '.json'), 'w') as out:
-        print(f'Got {len(result)} bytes back')
         out.write(result)
+        stats = element_stats(json.loads(result)['elements'])
+        rels = stats.get('relation') or 0
+        ways = stats.get('way') or 0
+        nodes = stats.get('node') or 0
+        print(f'  -> {len(result)} bytes, rel={rels}, way={ways}, node={nodes}')
