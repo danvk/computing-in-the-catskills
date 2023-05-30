b505e3f0ebddfc5344ef0953be5f2fa25f9652f0
Dan Vanderkam <danvdk@gmail.com>
2023-05-30 16:11:48 -0400

    Add Fir access from Burnham Hollow

--- augment_trails.py
+++ augment_trails.py
@@ -10,14 +10,18 @@ files = [
     'data/balsam-cap-to-table.geojson',
     'data/slide-cornell-to-friday.geojson',
     'data/north-dome-to-devils-path.geojson',
+    'data/burnham-hollow-fir.geojson',
 ]
 
 
-trail_elements: List[OsmElement] = json.load(open('data/trails.json'))['elements']
-trail_ways = [el for el in trail_elements if el['type'] == 'way']
-trail_nodes = {
+osm_elements: List[OsmElement] = (
+    json.load(open('data/trails.json'))['elements'] +
+    json.load(open('data/roads.json'))['elements']
+)
+osm_ways = [el for el in osm_elements if el['type'] == 'way']
+osm_nodes = {
     el['id']: el
-    for el in trail_elements
+    for el in osm_elements
     if el['type'] == 'node'
 }
 
@@ -39,11 +43,12 @@ for file in files:
     assert f['geometry']['type'] == 'LineString'
     coords = f['geometry']['coordinates']
 
-    # Force the trail to be connected by adding the closest nodes from other trails on either end.
-    d, start_node = closest_point_on_trail(coords[0][:2], trail_ways, trail_nodes)
+    # Force the trail to be connected by adding the closest nodes from other
+    # trails/roads on either end.
+    d, start_node = closest_point_on_trail(coords[0][:2], osm_ways, osm_nodes)
     assert d < 50
 
-    d, end_node = closest_point_on_trail(coords[-1][:2], trail_ways, trail_nodes)
+    d, end_node = closest_point_on_trail(coords[-1][:2], osm_ways, osm_nodes)
     assert d < 50, f'{coords[-1][:2]} is {d} meters from {end_node}'
 
     nodes: List[OsmNode] = [
@@ -76,4 +81,4 @@ with open('data/additional-trails.json', 'w') as out:
     json.dump({'elements': elements}, out, indent=2)
 
 with open('data/combined-trails.json', 'w') as out:
-    json.dump({'elements': trail_elements + elements}, out, indent=2)
+    json.dump({'elements': osm_elements + elements}, out, indent=2)
