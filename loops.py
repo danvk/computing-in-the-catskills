27ec87e1eb6abed920ad634244c29750f4160a09
Dan Vanderkam <danvdk@gmail.com>
2023-06-03 16:37:27 -0400

    624 trailhead/peak pairs

--- loops.py
+++ loops.py
@@ -26,3 +26,23 @@ features = json.load(open('data/network.geojson'))['features']
 G, id_to_peak, id_to_trailhead = read_hiking_graph(features)
 peak_features = [f for f in features if f['properties'].get('type') == 'high-peak']
 
+trail_g = nx.Graph()
+
+for th_node in G.nodes():
+    if G.nodes[th_node]['type'] != 'trailhead':
+        continue
+
+    lengths, paths = nx.single_source_dijkstra(G, th_node)
+    for node, length in lengths.items():
+        if G.nodes[node]['type'] == 'trailhead':
+            continue  # we want to disallow trailhead->trailhead travel
+        trail_g.add_edge(th_node, (th_node, node), weight=length, path=paths[node])
+
+tuples = [n for n in trail_g.nodes() if isinstance(n, tuple) and G.nodes[n[1]]['type'] == 'high-peak']
+# 624 trailhead/peak tuples
+print(f'{len(tuples)} trailhead/peak tuples')
+
+# for trailhead_id, peak_id in tuples:
+#     th = id_to_trailhead[trailhead_id]
+#     peak = id_to_peak[peak_id]
+#     print(f'  {trailhead_id}, {peak_id} = {th["properties"].get("name")}, {peak["properties"].get("name")}')
